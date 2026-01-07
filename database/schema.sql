3b07378fc2b24bcc21a94fa1482f5a29c5dd1893
DilshaKalpani <sheharadilsha035@gmail.com>
2026-01-07 15:14:34 +0530

    Update database schema and sample data

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,70 @@
-//vidya
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('ADMIN', 'STUDENT', 'LECTURER') NOT NULL
+);
+
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL,
+    hod VARCHAR(100),
+    staff_count INT
+);
+
+
+CREATE TABLE degrees (
+    degree_id INT AUTO_INCREMENT PRIMARY KEY,
+    degree_name VARCHAR(100) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    registration_number VARCHAR(20) UNIQUE NOT NULL,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    mobile VARCHAR(15),
+    degree_id INT,
+    user_id INT,
+    FOREIGN KEY (degree_id) REFERENCES degrees(degree_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE lecturers (
+    lecturer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    mobile VARCHAR(15),
+    department_id INT,
+    user_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_code VARCHAR(20) UNIQUE NOT NULL,
+    course_name VARCHAR(100),
+    credit VARCHAR(10),
+    lecturer_id INT,
+    FOREIGN KEY (lecturer_id) REFERENCES lecturers(lecturer_id)
+);
+
+
+CREATE TABLE enrollment (
+    enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    grade VARCHAR(5),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+
+ALTER TABLE courses
+ADD COLUMN day_of_week ENUM('Monday','Tuesday','Wednesday','Thursday','Friday'),
+ADD COLUMN start_time TIME,
+ADD COLUMN end_time TIME;
